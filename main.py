40fcf1c3dfc1212db2f2726c79840c59afded281
gemmaporrill <32202279-gemmaporrill@users.noreply.replit.com>
2024-02-19 14:28:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+# farenheit to celcius
+
+# temp = float(input("Enter temperature in Fahrenheit: "))
+# celcius = (temp - 32) * 5/9
+# print(celcius)
+
+# age of person if year is given
+
+# from datetime import date
+
+# year = int(input("Enter year of birth: "))
+# today = date.today()
+# age = today.year - year
+# print(f"If the year you were born in is {year}, then you are {age} years old")
+
+#area of the circle 
+
+# radius = float(input("Enter radius of circle: "))
+# area = 3.14 * radius ** 2
+# print(area)
+
+#progress bar when percentage is input
+
+# loader = int(input("Enter percentage: "))
+# done = loader // 10
+# left_over = 10 - done
+# print(f'[{"=" * done}{" " * left_over}] {loader}%')
+
+#isolating a string
+
+quote = "I love python"
+print(quote[0])
+print(quote[7:])
+
+# print(quote[100]) index out of range 
+
+#reverse first character
+print(quote[::-1])
+
+
+
+
+
