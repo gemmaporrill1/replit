cfe64c3524813016cd670a646e4189ac8e31af33
gemmaporrill1 <160475176+gemmaporrill1@users.noreply.github.com>
2024-02-21 12:48:32 +0000

    nested dictionary file added and started

--- dictionary.py
+++ dictionary.py
@@ -5,32 +5,50 @@
 # () | Tuple
 # {} | Dictionary
 
+# person = {
+#   "name": "Siya Kolisi",
+#   "age": 32,
+#   "country": "SA",
+#   "sport": "Rugby"
+# }
+
+# #access
+# print(person, type(person))
+# print(person["name"])
+
+# #update
+# person["age"] += 1 
+# print(person["age"])
+
+# #methods
+# # Iterable -> list, tuple, dict_keys (can use for loop)
+# print(person.keys()) #gives key titles
+# print(person.values()) #gives key values
+# print(person.items()) #tuple of items -> can use unpacking , using with a for loop
+
+# #Looping a dictionary
+
+# for detail in person.items():
+#   print(detail[0]) #[0] gives you just the keys or 1 gives you values
+
+# #unpacking a dictionary
+# for key, value in person.items(): #give proper name
+#   print(key, value)
+
+
+# safety for your values
+
 person = {
   "name": "Siya Kolisi",
   "age": 32,
   "country": "SA",
-  "sport": "Rugby"
+  "sport": "Rugby",
+  "height": 186
 }
 
-#access
-print(person, type(person))
-print(person["name"])
+# print(person['height']) #error no height in keys 
 
-#update
-person["age"] += 1 
-print(person["age"])
+print(person.get('height')) #gives none if not in keys
 
-#methods
-# Iterable -> list, tuple, dict_keys (can use for loop)
-print(person.keys()) #gives key titles
-print(person.values()) #gives key values
-print(person.items()) #tuple of items -> can use unpacking , using with a for loop
-
-#Looping a dictionary
-
-for detail in person.items():
-  print(detail[0]) #[0] gives you just the keys or 1 gives you values
-
-#unpacking a dictionary
-for key, value in person.items(): #give proper name
-  print(key, value)
+#get('key', defualt_value)
+print(person.get('height', 175)) #prints default value only if not present 
---
+++ nested_dict.py
@@ -0,0 +1,12 @@
+person = {
+  "name": "Siya Kolisi",
+  "age": 32,
+  "address": {
+    "city": "Port Elizabeth",
+    "country": "SA",
+  }, 
+  "sport": "Rugby",
+  "height": 186
+}
+
+print(person["address"]["city"])
