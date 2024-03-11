4a2ab79cdbce529611dbc0376d92eb9711995d9a
gemmaporrill1 <gemma.porrill@sanlam.co.za>
2024-03-11 08:57:47 +0200

    summarised post task

--- day_15_json_ex.py
+++ day_15_json_ex.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 
 # task 1
 # posts_summary.json
@@ -7,11 +8,17 @@ with open("./blog_post.json", "r") as file:
     blog_data = json.load(file)
     print(blog_data, type(blog_data))
 
-def extraction():
-    for post in blog_data:
-        
+
+def post_summary():
+    blog_posts_summary = [
+        {posts: post for posts, post in post.items() if posts != "comments"}
+        for post in blog_data["posts"]
+    ]
+    return blog_posts_summary
 
 
+with open("posts_summary.json", "w") as file:
+    extracted_posts = post_summary()
+    json.dump({"posts": extracted_posts}, file, indent=4)
 
-# with open("posts_summary.json", "w") as file:
-#     json.dump(blog_posts, file, indent=3)
+pprint(post_summary())
--- json.md
+++ json.md
@@ -1,9 +1,10 @@
 # JSON
-- universal language
+- universal language -> understood by any language because it's a string
 - loose coupling - if you modify backend, frontend doesn't worry about it as long as it's JSON | server and front end are independent 
-- platform independent
+- platform independent -> android, apple, web it will understand 
 
 - micro service architecture
+- monolith architecture
 
 - read and write -> open()
 - r
